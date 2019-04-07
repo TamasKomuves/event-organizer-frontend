da131785eabd967f5c303c388f0a8c51c5f0f81a
Tamas Komuves <komuves.tomi@gmail.com>
2019-04-07 23:17:04 +0200

    Fix update bug for modals

--- src/app/modals/event-participants-modal/event-participants-modal.component.ts
+++ src/app/modals/event-participants-modal/event-participants-modal.component.ts
@@ -1,21 +1,30 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, AfterViewInit } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
+import { NgxSmartModalService, NgxSmartModalComponent } from 'ngx-smart-modal';
 
 @Component({
   selector: 'app-event-participants-modal',
   templateUrl: './event-participants-modal.component.html',
   styleUrls: ['./event-participants-modal.component.css']
 })
-export class EventParticipantsModalComponent implements OnInit {
+export class EventParticipantsModalComponent implements AfterViewInit {
   @Input() eventId;
 
   participants: any;
 
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private ngxSmartModalService: NgxSmartModalService) {}
 
-  ngOnInit() {
-    this.userService.getEventParticipants(this.eventId).subscribe(participants => {
-      this.participants = participants;
+  ngAfterViewInit() {
+    const eventParticipantsModal = this.ngxSmartModalService.getModal('eventParticipantsModal');
+
+    eventParticipantsModal.onOpen.subscribe((modal: NgxSmartModalComponent) => {
+      this.userService.getEventParticipants(this.eventId).subscribe(participants => {
+        this.participants = participants;
+      });
+    });
+
+    eventParticipantsModal.onClose.subscribe((modal: NgxSmartModalComponent) => {
+      this.participants = [];
     });
   }
 }
--- src/app/modals/view-request-modal/view-request-modal.component.ts
+++ src/app/modals/view-request-modal/view-request-modal.component.ts
@@ -1,21 +1,30 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, AfterViewInit } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
+import { NgxSmartModalComponent, NgxSmartModalService } from 'ngx-smart-modal';
 
 @Component({
   selector: 'app-view-request-modal',
   templateUrl: './view-request-modal.component.html',
   styleUrls: ['./view-request-modal.component.css']
 })
-export class ViewRequestModalComponent implements OnInit {
+export class ViewRequestModalComponent implements AfterViewInit {
   @Input() eventId;
 
   invitations: any;
 
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private ngxSmartModalService: NgxSmartModalService) {}
 
-  ngOnInit() {
-    this.userService.getInvitationRequestsForEvent(this.eventId).subscribe(invitations => {
-      this.invitations = invitations;
+  ngAfterViewInit() {
+    const invitationRequestModal = this.ngxSmartModalService.getModal('invitationRequestModal');
+
+    invitationRequestModal.onOpen.subscribe((modal: NgxSmartModalComponent) => {
+      this.userService.getInvitationRequestsForEvent(this.eventId).subscribe(invitations => {
+        this.invitations = invitations;
+      });
+    });
+
+    invitationRequestModal.onClose.subscribe((modal: NgxSmartModalComponent) => {
+      this.invitations = [];
     });
   }
 }
