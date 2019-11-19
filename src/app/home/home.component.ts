86eac988cfc14ccc742210442a02c89c3e98e100
Tamas Komuves <komuves.tomi@gmail.com>
2019-11-19 21:12:28 +0100

    Introduce working websocket example

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
+import { Message } from '../interface/Message';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  stompClient: any;
+  isLoaded = false;
+  isCustomSocketOpened = false;
+  messages;
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    const ws = new SockJS('http://localhost:8080/socket/?t=' + sessionStorage.getItem('token'));
+    this.stompClient = Stomp.over(ws);
+    const that = this;
+    this.stompClient.connect({}, function(frame) {
+      that.isLoaded = true;
+      that.openGlobalSocket();
+    });
+  }
+
+  openGlobalSocket() {
+    this.stompClient.subscribe('/socket-publisher', message => {
+      this.handleResult(message);
+    });
+  }
+
+  openSocket() {
+    if (this.isLoaded) {
+      this.isCustomSocketOpened = true;
+      this.stompClient.subscribe('/socket-publisher/' + '12', message => {
+        this.handleResult(message);
+      });
+    }
+  }
+
+  handleResult(message) {
+    if (message.body) {
+      const messageResult: Message = JSON.parse(message.body);
+      console.log(messageResult);
+      this.messages.push(messageResult);
+    }
+  }
+
+  sendMessageUsingSocket() {
+    const message: Message = { message: 'csacsa', fromId: '12', toId: '13' };
+    this.stompClient.send('/socket-subscriber/send/message', {}, JSON.stringify(message));
+  }
 }
