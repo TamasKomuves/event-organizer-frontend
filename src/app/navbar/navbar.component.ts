ee44fd8a6337a1b32d48e8302e51f235c96bf657
Tamas Komuves <komuves.tomi@gmail.com>
2019-12-01 00:31:45 +0100

    Get notificationCounter everytime after login

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { MessageService } from '../services/message.service';
 import { UserService } from '../services/user.service';
 import { Router } from '@angular/router';
+import { WebsocketService } from '../services/websocket.service';
+import { IUser } from '../interface/IUser';
 
 @Component({
   selector: 'app-navbar',
@@ -16,23 +18,35 @@ export class NavbarComponent implements OnInit {
   constructor(
     private messageService: MessageService,
     private userService: UserService,
-    private router: Router
+    private router: Router,
+    private websocketService: WebsocketService
   ) {}
 
   ngOnInit() {
     this.userService.getCurrentUser().subscribe(
-      data => {
+      (user: IUser) => {
         this.isLoggedIn = true;
+        this.websocketService.connect(
+          '/socket-publisher/invitation-counter/' + user.email,
+          socketMessage => {
+            this.notificationCounter++;
+          }
+        );
       },
       error => {
         this.isLoggedIn = false;
       }
     );
-    this.messageService.getLoggedInMessage().subscribe(message => {
-      this.isLoggedIn = message.isLoggedIn;
-    });
     this.userService.getNotSeenNotificationCount().subscribe((result: number) => {
       this.notificationCounter = result;
+    });
+    this.messageService.getLoggedInMessage().subscribe(message => {
+      this.isLoggedIn = message.isLoggedIn;
+      if (message.isLoggedIn) {
+        this.userService.getNotSeenNotificationCount().subscribe((result: number) => {
+          this.notificationCounter = result;
+        });
+      }
     });
   }
 
