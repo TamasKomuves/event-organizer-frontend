5740d8c95dff1ab8107768736ce89e01118826f2
Tamas Komuves <komuves.tomi@gmail.com>
2020-01-19 18:17:45 +0100

    Rename SERVER_LINK to SERVER_BASE_URL in BaseRestService

--- src/app/services/rest/base-rest.service.ts
+++ src/app/services/rest/base-rest.service.ts
@@ -1,19 +1,19 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 export abstract class BaseRestService {
-  // SERVER_LINK = 'https://powerful-shelf-66888.herokuapp.com/';
-  SERVER_LINK = 'http://localhost:8080/';
+  // SERVER_BASE_URL = 'https://powerful-shelf-66888.herokuapp.com/';
+  SERVER_BASE_URL = 'http://localhost:8080/';
 
   constructor(private httpClient: HttpClient) {}
 
   getMethod<T>(url: string) {
-    return this.httpClient.get<T>(this.SERVER_LINK + url, {
+    return this.httpClient.get<T>(this.SERVER_BASE_URL + url, {
       headers: new HttpHeaders().set('authorization', 'Bearer ' + sessionStorage.getItem('token'))
     });
   }
 
   postMethod<T>(url: string, body: any) {
-    return this.httpClient.post<T>(this.SERVER_LINK + url, body, {
+    return this.httpClient.post<T>(this.SERVER_BASE_URL + url, body, {
       headers: new HttpHeaders()
         .set('content-type', 'application/json')
         .set('authorization', 'Bearer ' + sessionStorage.getItem('token'))
@@ -21,13 +21,13 @@ export abstract class BaseRestService {
   }
 
   deleteMethod(url: string) {
-    return this.httpClient.delete(this.SERVER_LINK + url, {
+    return this.httpClient.delete(this.SERVER_BASE_URL + url, {
       headers: new HttpHeaders().set('authorization', 'Bearer ' + sessionStorage.getItem('token'))
     });
   }
 
   putMethod<T>(url: string, body: any) {
-    return this.httpClient.put<T>(this.SERVER_LINK + url, body, {
+    return this.httpClient.put<T>(this.SERVER_BASE_URL + url, body, {
       headers: new HttpHeaders()
         .set('content-type', 'application/json')
         .set('authorization', 'Bearer ' + sessionStorage.getItem('token'))
@@ -35,7 +35,7 @@ export abstract class BaseRestService {
   }
 
   postMethodWithoutAuth(url: string, body: any) {
-    return this.httpClient.post(this.SERVER_LINK + url, body, {
+    return this.httpClient.post(this.SERVER_BASE_URL + url, body, {
       headers: new HttpHeaders().set('content-type', 'application/json')
     });
   }
