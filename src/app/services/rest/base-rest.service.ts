4c9d398fca062e6ac7666f84c334a180ff0c8dc3
Tamas Komuves <komuves.tomi@gmail.com>
2020-01-31 00:39:19 +0100

    Use environment variable to store api url

--- src/app/services/rest/base-rest.service.ts
+++ src/app/services/rest/base-rest.service.ts
@@ -1,19 +1,17 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 export abstract class BaseRestService {
-  // SERVER_BASE_URL = 'https://powerful-shelf-66888.herokuapp.com/';
-  SERVER_BASE_URL = 'http://localhost:8080/';
-
   constructor(private httpClient: HttpClient) {}
 
   getMethod<T>(url: string) {
-    return this.httpClient.get<T>(this.SERVER_BASE_URL + url, {
+    return this.httpClient.get<T>(environment.apiUrl + url, {
       headers: new HttpHeaders().set('authorization', 'Bearer ' + sessionStorage.getItem('token'))
     });
   }
 
   postMethod<T>(url: string, body: any) {
-    return this.httpClient.post<T>(this.SERVER_BASE_URL + url, body, {
+    return this.httpClient.post<T>(environment.apiUrl + url, body, {
       headers: new HttpHeaders()
         .set('content-type', 'application/json')
         .set('authorization', 'Bearer ' + sessionStorage.getItem('token'))
@@ -21,13 +19,13 @@ export abstract class BaseRestService {
   }
 
   deleteMethod(url: string) {
-    return this.httpClient.delete(this.SERVER_BASE_URL + url, {
+    return this.httpClient.delete(environment.apiUrl + url, {
       headers: new HttpHeaders().set('authorization', 'Bearer ' + sessionStorage.getItem('token'))
     });
   }
 
   putMethod<T>(url: string, body: any) {
-    return this.httpClient.put<T>(this.SERVER_BASE_URL + url, body, {
+    return this.httpClient.put<T>(environment.apiUrl + url, body, {
       headers: new HttpHeaders()
         .set('content-type', 'application/json')
         .set('authorization', 'Bearer ' + sessionStorage.getItem('token'))
@@ -35,7 +33,7 @@ export abstract class BaseRestService {
   }
 
   postMethodWithoutAuth(url: string, body: any) {
-    return this.httpClient.post(this.SERVER_BASE_URL + url, body, {
+    return this.httpClient.post(environment.apiUrl + url, body, {
       headers: new HttpHeaders().set('content-type', 'application/json')
     });
   }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://powerful-shelf-66888.herokuapp.com/'
 };
