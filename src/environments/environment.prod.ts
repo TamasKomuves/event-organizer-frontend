7ab19cf6539ecfd52336d0c2a3b8711428071ead
Tamas Komuves <komuves.tomi@gmail.com>
2020-03-13 20:38:36 +0100

    Refer to renamed backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://powerful-shelf-66888.herokuapp.com/',
+  apiUrl: 'https://event-organizer-hun-backend.herokuapp.com/',
   dialogflow: {
     angularBot: 'd679e14cd817499baeb199930578d716'
   }
